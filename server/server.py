477f309c7ef57da26fe1a20c240896d20fff682a
Woradorn K <chateau86@gmail.com>
2020-02-27 19:55:54 -0500

    Added basic server code

---
+++ server/server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+print("Server code goes here")
+
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>It's alive!!!</h1>"
+
+app.run()
