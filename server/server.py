952797c3f25d5e9dd1b456f23644960c1af5ce19
Woradorn K <chateau86@gmail.com>
2020-02-27 21:01:51 -0500

    Added flask DB stuff

--- server/server.py
+++ server/server.py
@@ -2,13 +2,17 @@
 
 # run `flask run --host=0.0.0.0`
 
-print("Server code goes here")
+import os
 
 import flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app.config["DEBUG"] = True,
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URL']
 
+print("Server init ok")
+print("DB URL: "+os.environ['DB_URL'])
 
 @app.route('/', methods=['GET'])
 def home():
