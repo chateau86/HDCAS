c2fa6dfebafe2a172289dbb6b63b99000e06a40f
Woradorn Kamolpornwijit <chateau86@gmail.com>
2020-04-11 01:13:34 -0400

    Added client stuff (#1)
    Added client code

--- client/client.py
+++ client/client.py
@@ -1,62 +1,42 @@
 #!/usr/bin/env python3
 
-from tkinter import Tk, Frame, Listbox, Scrollbar, Label, Button
-from pySMART import DeviceList
+from tkinter import Tk, Frame, Label, Button, scrolledtext, Toplevel  # noqa: E501
+
+import queue
 import ctypes
 import os
 import sys
+import requests
+# import pprint
+
+from worker import SMARTReaderThread, DriveStatusTransmitterThread, DriveStatusRecieverThread  # noqa: E501
+from utils import DriveInfoRequestPayload, WarningItem, DriveItem, AttrItem  # noqa: E501
+from utils import ListWithScroll, EntryWithLabel, SelectWithLabel, CheckWithLabel  # noqa: E501
+from utils import SURROGATE_PAIRS
 
 
-class WarningItem:
-    def __init__(self, title, desc):
-        self.title = title
-        self.desc = desc
-
-    def __repr__(self):
-        return self.title + ": " + self.desc
-
-
-class DriveItem:
-    def __init__(self, title, smart_data, warnings):
-        self.title = title
-        self.smart_data = smart_data
-        self.warnings = warnings
-
-    def __repr__(self):
-        out = self.title + '\n' + \
-              '\tSMART: ' + self.smart_data
-        for warn in self.warnings:
-            out += '\t\t' + str(warn)
-        return out
+BACKEND_URL = "http://10.0.0.115:5000"
+# API_TOKEN = "c91a3ec3-0ad2-471e-8899-3211da76074f"  # test
+# API_TOKEN = "a72fbda1-4cbc-4e0f-b457-d26cab89e125"  # chateau86
+# API_TOKEN = "d72aea55-1c05-41c5-a9dd-3c3c0c0e8e2d"  # backblaze
+API_TOKEN = "d065ad28-fe88-4edf-95bb-8663605659d2"  # backblaze_demo
+# TODO: Ask user for token
 
 
-class ListWithScroll:
-    def __init__(self, parent, callback):
-        self._frame = Frame(parent)
-        self._scrollbar = Scrollbar(self._frame)
-        self._list_box = Listbox(self._frame, selectmode="browse",
-                                 exportselection=False,
-                                 yscrollcommand=self._scrollbar.set)
-        self._scrollbar.config(command=self._list_box.yview)
-        self._list_box.pack(side='left', fill='both', expand=1)
-        self._scrollbar.pack(side='right', fill='both')
-        self._list_box.bind('<<ListboxSelect>>', callback)
-
-    def get_list_box(self):
-        return self._list_box
-
-    def get_frame(self):
-        return self._frame
-
-
-class Main_window(Frame):
+class MainWindow(Frame):
     def __init__(self, master=None):
         self.drive_list = []
+        self.drive_dict = {}
         self.current_drive = None
 
         self.master = master
         self.master.title("HDCAS")
-        self.master.minsize(height=200, width=200)
+        self.master.minsize(height=200, width=600)
+
+        self.smart_device_queue = queue.Queue()
+        self.network_pull_queue = queue.Queue()
+        self.network_send_response_queue = queue.Queue()
+        self.child_window = None
 
         top_controls_frame = Frame(master)
         drive_list_frame = Frame(master)
@@ -64,7 +44,8 @@ class Main_window(Frame):
         top_controls_frame.grid(sticky='nsew', row=0, column=0)
         drive_list_frame.grid(sticky="nsew", row=1, column=0)
         drive_detail_frame.grid(sticky="nsew", row=2, column=0)
-        master.grid_rowconfigure(1, weight=1)
+        master.grid_rowconfigure(0, weight=0)
+        master.grid_rowconfigure(1, weight=0)
         master.grid_rowconfigure(2, weight=1)
         master.grid_columnconfigure(0, weight=1)
 
@@ -76,6 +57,14 @@ class Main_window(Frame):
         btn = Button(top_controls_frame,
                      text="Load SMART data",
                      command=self.do_SMART_read)
+        btn.pack(side='left')
+        btn = Button(top_controls_frame,
+                     text="Download data",
+                     command=self.do_network_pull)
+        btn.pack(side='left')
+        btn = Button(top_controls_frame,
+                     text="Register/update selected drive",
+                     command=self.do_drive_register)
         btn.pack(side='left')
 
         # init drive list frame
@@ -92,7 +81,6 @@ class Main_window(Frame):
                           )
         drive_msg.grid(row=0, column=0, sticky='nsew')
         drive_detail_frame.grid_columnconfigure(0, weight=1)
-
         self._drive_msg = drive_msg
 
         warning_list = ListWithScroll(drive_detail_frame,
@@ -101,22 +89,39 @@ class Main_window(Frame):
 
         warning_detail_frame = Frame(drive_detail_frame)
         warning_detail_frame.grid(sticky="nsew", row=2, column=0)
-        warning_msg = Label(warning_detail_frame,
-                            text="Drive warning here",
-                            anchor='w',
-                            )
-        warning_msg.grid(row=0, column=0, sticky='nsew')
+        drive_detail_frame.grid_rowconfigure(2, weight=1)
+        warning_msg_label = Label(warning_detail_frame,
+                                  text="Warning details",
+                                  anchor='w',)
+        warning_msg_label.grid(row=0, column=0, sticky='nsew')
+        warning_msg = scrolledtext.ScrolledText(
+            warning_detail_frame,
+            state='disabled',
+            width=40, height=10,
+            wrap='word',
+        )
+        warning_msg.grid(row=1, column=0, sticky='nsew')
+        warning_detail_frame.grid_rowconfigure(1, weight=1)
         warning_detail_frame.grid_columnconfigure(0, weight=1)
 
         self._warning_list = warning_list.get_list_box()
         self._warning_msg = warning_msg
+        self._watch_smart_data_queue()
+        self._watch_net_send_resp_queue()
+        self._watch_net_recv_resp_queue()
 
-    def _update_drive_list(self, drive_list):
+    def _update_drive_list(self):
+        # now sort the dict
+        drive_list = []
+        for sn in self.drive_dict:
+            drive_list.append(self.drive_dict[sn])
+        drive_list.sort(key=lambda x: x.ui_sort_key())
+        # print(drive_list)
         self._drive_list.delete(0, 'end')
         self.drive_list = drive_list
         self.current_drive = None
         for i in range(len(self.drive_list)):
-            self._drive_list.insert(i, self.drive_list[i].title)
+            self._drive_list.insert(i, self.drive_list[i].get_display_name())
         self._update_warn_box()
 
     def _drive_click_callback(self, event):
@@ -127,7 +132,7 @@ class Main_window(Frame):
         else:
             index = int(w.curselection()[0])
             self.current_drive = self.drive_list[index]
-            self._drive_msg.configure(text=str(self.current_drive.title))
+            self._drive_msg.configure(text=str(self.current_drive.get_display_name()))  # noqa: E501
         self._update_warn_box()
 
     def _update_warn_box(self):
@@ -135,43 +140,289 @@ class Main_window(Frame):
         if self.current_drive is not None:
             for i in range(len(self.current_drive.warnings)):
                 self._warning_list \
-                    .insert(i, self.current_drive.warnings[i].title)
-        self._warning_msg.configure(text="No warning selected")
+                    .insert(i, self.current_drive.warnings[i].get_display_name())  # noqa: E501
+        self._set_warning_msg("No warning selected")
 
     def _warning_click_callback(self, event):
         w = event.widget
         if len(w.curselection()) == 0:
             # self.current_drive = None
-            self._warning_msg.configure(text="No warning selected")
+            self._set_warning_msg("No warning selected")
             return
         index = int(w.curselection()[0])
-        self._warning_msg \
-            .configure(text=str(self.current_drive.warnings[index].desc))
+        self._set_warning_msg(str(self.current_drive.warnings[index].desc))
         return
 
-    def do_SMART_read(self):
-        # TODO: Actual SMART data read and stuff
-        # TODO: Async this
-        raw_drive_list = DeviceList()
-        print(raw_drive_list)
-        drive_list = []
-        for dev in raw_drive_list.devices:
-            param_list = []
-            for attr in dev.attributes:
+    def _set_warning_msg(self, msg):
+        self._warning_msg['state'] = 'normal'
+        self._warning_msg.delete('1.0', 'end')
+        self._warning_msg.insert('1.0', msg)
+        self._warning_msg['state'] = 'disabled'
+
+    def _watch_smart_data_queue(self):
+        # print("Watching SMART queue")
+        self.master.after(200, self._watch_smart_data_queue)
+        drive_list = {}
+        try:
+            drive_list = self.smart_device_queue.get_nowait()
+        except queue.Empty:
+            return
+        drive_item_dict = self.drive_dict
+        drive_request_dict = {}
+        for serial in drive_list:
+            drive = drive_list[serial]
+            attr_list = {}
+            for attr in drive.attributes:
                 if attr is None:
                     continue
-                param_list.append(
-                    WarningItem(attr.name, attr.name + ": " + attr.raw)
+                attr_list[attr.num] = AttrItem(
+                    attr.num,
+                    attr.name,
+                    attr.raw,
+                    attr.value
                 )
-            drive_list.append(
-                DriveItem(dev.model + ": " + dev.serial, str(dev), param_list)
+            disp_string = ""
+            for attr in attr_list:
+                disp_string += str(attr_list[attr]) + '\n'
+            # implicitly also discard all past warnings
+            # as they would no longer be valid
+            warning_list = [
+                WarningItem("Raw SMART readings", disp_string),
+            ]
+            if drive.serial not in drive_item_dict:
+                drive_item_dict[drive.serial] = DriveItem(
+                    str(drive),
+                    warning_list,
+                    drive.serial,
+                    drive.model,
+                    is_ssd=drive.is_ssd,
+                )
+            else:
+                drive_item_dict[drive.serial].warnings = warning_list
+            drive_request_dict[serial] = DriveInfoRequestPayload(
+                model=drive.model,
+                serial=drive.serial,
+                is_ssd=drive.is_ssd,
+                attr_list=attr_list,
+                timestamp_override=None,
             )
-        self._update_drive_list(drive_list)
+        self.drive_dict = drive_item_dict
+        self._update_drive_list()
+        self.do_network_push(drive_request_dict)
+
+    def _watch_net_send_resp_queue(self):
+        # print("Watching SMART queue")
+        self.master.after(200, self._watch_net_send_resp_queue)
+        try:
+            _ = self.network_send_response_queue.get_nowait()
+        except queue.Empty:
+            return
+        # TODO
+
+    def _watch_net_recv_resp_queue(self):
+        # print("Watching SMART queue")
+        self.master.after(200, self._watch_net_recv_resp_queue)
+        try:
+            info_dict, warning_dict = self.network_pull_queue.get_nowait()
+        except queue.Empty:
+            return
+        # pprint.pprint(info_dict)
+        # pprint.pprint(warning_dict)
+        for sn in info_dict:
+            if sn not in self.drive_dict:
+                self.drive_dict[sn] = DriveItem(
+                    "",
+                    [],
+                    sn,
+                    info_dict[sn]['drive_model'],
+                    is_registered=True,
+                    nickname=info_dict[sn]['drive_nickname'],
+                    status=info_dict[sn]['drive_status'],
+                )
+            else:
+                self.drive_dict[sn].is_registered = True
+                self.drive_dict[sn].model = info_dict[sn]['drive_model']
+                self.drive_dict[sn].nickname = info_dict[sn]['drive_nickname']
+                self.drive_dict[sn].status = info_dict[sn]['drive_status']
+        # Read warning
+        for sn in warning_dict:
+            warning_list = []
+            warnings_dict = warning_dict[sn]['response_json']
+            for algo in warnings_dict:
+                res = warning_dict[sn]['response_json'][algo]
+                # pprint.pprint(res)
+                warning_msg = "Algorithm: {:}\n".format(algo)
+                warning_msg += "\tAlgorithm version: {:}\n".format(res['version'])  # noqa: E501
+                warning_msg += "\tTraining data date: {:}\n".format(res['data_date'])  # noqa: E501
+                for item in res['warn_list']:
+                    warning_msg += "{:}{:}\n".format(SURROGATE_PAIRS[item['level']], item['name'])  # noqa: E501
+                    warning_msg += "\tDescription: {:}\n".format(item['desc'])
+                    warning_msg += "\tValue: {:}\n".format(item['value'])
+                warning_list.append(
+                    WarningItem(
+                        algo,
+                        warning_msg,
+                        color=res['level'],
+                    ),
+                )
+            self.drive_dict[sn].warnings = warning_list
+        # print("net recv ok")
+        self._update_drive_list()
+
+    def do_SMART_read(self):
+        worker = SMARTReaderThread(self.smart_device_queue)
+        worker.run()
+
+    def do_network_push(self, drive_request_dict):
+        worker = DriveStatusTransmitterThread(self.network_send_response_queue, BACKEND_URL, API_TOKEN)  # noqa: E501
+        worker.run(drive_request_dict)
+
+    def do_drive_register(self):
+        # TODO:
+        if self.child_window:
+            self.child_window.destroy()
+            self.child_window = None
+        else:
+            if self.current_drive is not None:
+                self.child_window = DriveRegisterWindow(
+                    cancel_callback=self._clear_child_window,
+                    serial=self.current_drive.serial,
+                    drive_item=self.drive_dict[self.current_drive.serial],
+                )
+            else:
+                self.child_window = DriveRegisterWindow(
+                    cancel_callback=self._clear_child_window,
+                )
+
+    def _clear_child_window(self):
+        self.child_window = None
+        self.do_network_pull()
+
+    def do_network_pull(self):
+        worker = DriveStatusRecieverThread(self.network_pull_queue, BACKEND_URL, API_TOKEN)  # noqa: E501
+        worker.run()
 
     def clear_data(self):
-        # TODO: Actual SMART data read and stuff
-        drive_list = []
+        drive_list = {}
         self._update_drive_list(drive_list)
+
+
+class DriveRegisterWindow(Toplevel):
+    def __init__(self, cancel_callback, serial="", drive_item=None):
+        self._cancel_callback = cancel_callback
+        super().__init__(root)
+        self.protocol("WM_DELETE_WINDOW", self._cancel)
+
+        box_frames = Frame(self)
+        box_frames.pack()
+
+        self.model_entry = EntryWithLabel(box_frames, 0, "Model", "")
+        self.serial_entry = EntryWithLabel(box_frames, 1, "Serial Number", serial)  # noqa: E501
+        self.nickname_entry = EntryWithLabel(box_frames, 2, "Nickname", "")
+        self.status_entry = SelectWithLabel(box_frames, 3, "Drive status", ["Active", "Retired", "Failed"])  # noqa: E501
+        self.total_size_entry = EntryWithLabel(box_frames, 4, "Total size (bytes)", 0, self._is_int)  # noqa: E501
+        self.lba_size_entry = EntryWithLabel(box_frames, 5, "LBA size (bytes)", 512, self._is_int)  # noqa: E501
+        self.is_ssd_box = CheckWithLabel(box_frames, 6, "Drive is SSD")
+
+        btn_frame = Frame(self)
+        btn = Button(btn_frame,
+                     text="Submit",
+                     command=self._submit)
+        btn.pack(side='left')
+        btn = Button(btn_frame,
+                     text="Cancel",
+                     command=self._cancel)
+        btn.pack(side='left')
+        btn_frame.pack()
+        # TODO: Load drive info from server after form is loaded
+        if drive_item is not None:
+            self.model_entry.set_val(drive_item.model)
+            self.is_ssd_box.set_val(drive_item.is_ssd)
+        self._fetch_drive()
+
+    def _fetch_drive(self):
+        try:
+            req_body = {
+                'token': API_TOKEN,
+                'serial_number': self.serial_entry.get_val(),
+            }
+            resp = requests.post(BACKEND_URL+'/get_drive_info', data=req_body)  # noqa: E501
+            if resp.status_code == 404:
+                return
+            if resp.status_code != 200:
+                print("Error {:}: {:}".format(resp.status_code, resp.reason))  # noqa: E501
+            else:
+                print("Drive info Request OK")
+                body = resp.json()
+                if 'error' in body:
+                    print("Error: {:}".format(body['error']))
+                    return
+                # pprint.pprint(body)
+        except Exception as e:
+            print(e)
+            return
+        self.model_entry.set_val(body['drive_model'])
+        if body['drive_nickname'] is not None:
+            self.nickname_entry.set_val(body['drive_nickname'])
+        self.status_entry.set_val(['active', 'retired', 'failed'].index(body['drive_status']))  # noqa: E501
+        self.total_size_entry.set_val(body['drive_total_size_byte'])
+        self.lba_size_entry.set_val(body['drive_lba_size_byte'])
+        self.is_ssd_box.set_val(body['is_ssd'])
+
+    def _submit(self):
+        try:
+            req_body = {
+                'token': API_TOKEN,
+                'serial_number': self.serial_entry.get_val(),
+            }
+            req_body['status'] = self.status_entry.get_val().lower()
+            if self.model_entry.get_val() != '':
+                req_body['model'] = self.model_entry.get_val()
+            if self.nickname_entry.get_val() != '':
+                req_body['nickname'] = self.nickname_entry.get_val()
+            size_str = self.total_size_entry.get_val()
+            try:
+                size = int(size_str)
+                if size > 0:
+                    req_body['total_size_byte'] = size
+            except:  # noqa: E722
+                pass
+            size_str = self.lba_size_entry.get_val()
+            try:
+                size = int(size_str)
+                if size > 0:
+                    req_body['lba_size_byte'] = size
+            except:  # noqa: E722
+                pass
+            print(req_body)
+            resp = requests.post(BACKEND_URL+'/update_drive_info', data=req_body)  # noqa: E501
+            if resp.status_code == 404:
+                return
+            if resp.status_code != 200:
+                print("Error {:}: {:}".format(resp.status_code, resp.reason))  # noqa: E501
+            else:
+                print("Drive info Request OK")
+                body = resp.json()
+                if 'error' in body:
+                    print("Error: {:}".format(body['error']))
+                # pprint.pprint(body)
+        except Exception as e:
+            print(e)
+        self._cancel()
+
+    def _cancel(self):
+        print("Cancel called")
+        self._cancel_callback()
+        self.destroy()
+
+    def _is_int(self, val):
+        if val == '':
+            return True
+        try:
+            int(val)
+            return True
+        except Exception:
+            return False
 
 
 if __name__ == '__main__':
@@ -197,5 +448,5 @@ if __name__ == '__main__':
         print("Must run as admin for smartctl usage")
         exit()
     root = Tk()
-    app = Main_window(root)
+    app = MainWindow(root)
     root.mainloop()
