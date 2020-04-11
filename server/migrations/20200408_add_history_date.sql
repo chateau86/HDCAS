b7a9a2cb66fe4e017bbfd187168fab8611172784
Woradorn Kamolpornwijit <chateau86@gmail.com>
2020-04-11 01:18:43 -0400

    Revert "Server code (#2)" (#3)
    This reverts commit 566e7e978cd43c8e393dfaff84bcf0e017dad177.

--- server/migrations/20200408_add_history_date.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE historical_data ADD created_at TIMESTAMP DEFAULT NOW();
-ALTER TABLE historical_data DROP drive_status;
-ALTER TABLE responses ADD CONSTRAINT UQ_response UNIQUE(serial_number, username);
